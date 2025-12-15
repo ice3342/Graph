3a5ac2b68af2239f3fbe4aa7a916d73a9c96d8e2
ice3342 <kravitz3342@gmail.com>
2025-12-15 22:27:59 +0200

    DONE!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import tkinter as tk
+import os
 
 def main():
     app = Application()
@@ -11,6 +12,21 @@ class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Graphline")
+
+#       load options
+        with open("options") as options:
+            # load the is_on option
+            self.is_on = options.readline()
+            # the self.is_on is reversed because the fuction dark_mode flips them
+            try:
+                if int(self.is_on.replace("is_on=", "")) == 0:
+                    self.is_on = 1
+                else:
+                    self.is_on = 0
+            except ValueError:
+                self.is_on = 1
+
+#       ------------
 
 #       Frames
         self.topbar = tk.Frame(self)
@@ -63,6 +79,32 @@ class Application(tk.Tk):
                                   width=7, height=1)
         self.dark_mode_btn.grid(row=0, column=3)
 #       -------------------------------
+#       Variables
+        self.bgs = [self.label.cget("bg"),
+                    self.clear_btn.cget("bg"),
+                    self.add_line_btn.cget("bg"),
+                    self.X_axis.cget("bg"),
+                    self.Y_axis.cget("bg"),
+                    self.dark_mode_btn.cget("bg"),
+                    self.cget("bg"),
+                    self.ax.get_facecolor(),
+                    self.ax.figure.get_facecolor(),
+                    self.canvas.get_tk_widget().cget("bg"),
+                    self.toolbar.cget("bg"),
+                    self.graph_frame.cget("bg"),
+                    self.topbar.cget("bg")]
+                    
+        self.fgs = [self.label.cget("fg"),
+                    self.clear_btn.cget("fg"),
+                    self.add_line_btn.cget("fg"),
+                    self.X_axis.cget("fg"),
+                    self.Y_axis.cget("fg"),
+                    self.dark_mode_btn.cget("fg")]
+#       ---------
+
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.dark_mode()
+
 # Functions
     def add_line(self, X_axis, Y_axis):
 #       get and split the axis on commos
@@ -107,24 +149,53 @@ class Application(tk.Tk):
 #   ------------------------
 # dark mode on-and-off
     def dark_mode(self):
-        is_on = 0
-        self.label.configure(fg="#ffffff", bg="#202020")
-        self.add_line_btn.configure(fg="#ffffff",bg="#202020")
-        self.clear_btn.configure(fg="#ffffff",bg="#202020")
-        self.X_axis.configure(fg="#ffffff",bg="#202020")
-        self.Y_axis.configure(fg="#ffffff",bg="#202020")
-        self.dark_mode_btn.configure(fg="#ffffff",bg="#202020")
-        self.configure(bg="#202020")
-        self.ax.set_facecolor("#202020")
-        self.ax.figure.set_facecolor("#303030")
-        self.canvas.get_tk_widget().configure(bg="#202020")
-        self.toolbar.configure(bg="#303030")
-        for button in self.toolbar.winfo_children():
-            button.configure(bg="#303030")
-        self.canvas.draw()
-        self.graph_frame.configure(bg="#202020")
-        self.topbar.configure(bg="#202020")
 
+        if self.is_on == 0:
+            self.label.configure(fg="#ffffff", bg="#202020")
+            self.add_line_btn.configure(fg="#ffffff",bg="#202020")
+            self.clear_btn.configure(fg="#ffffff",bg="#202020")
+            self.X_axis.configure(fg="#ffffff",bg="#202020")
+            self.Y_axis.configure(fg="#ffffff",bg="#202020")
+            self.dark_mode_btn.configure(fg="#ffffff",bg="#202020")
+            self.configure(bg="#202020")
+            self.ax.set_facecolor("#202020")
+            self.ax.figure.set_facecolor("#303030")
+            self.canvas.get_tk_widget().configure(bg="#202020")
+            self.toolbar.configure(bg="#303030")
+            for button in self.toolbar.winfo_children():
+                button.configure(bg="#303030")
+            self.canvas.draw()
+            self.graph_frame.configure(bg="#202020")
+            self.topbar.configure(bg="#202020")
+            self.is_on = 1
+            print("dark!")
+            print(self.is_on)
+        else:
+            self.label.configure(fg=self.fgs[0], bg=self.bgs[0])
+            self.add_line_btn.configure(fg=self.fgs[1],bg=self.bgs[1])
+            self.clear_btn.configure(fg=self.fgs[2],bg=self.bgs[2])
+            self.X_axis.configure(fg=self.fgs[3],bg=self.bgs[3])
+            self.Y_axis.configure(fg=self.fgs[4],bg=self.bgs[4])
+            self.dark_mode_btn.configure(fg=self.fgs[5],bg=self.bgs[5])
+            self.configure(bg=self.bgs[6])
+            self.ax.set_facecolor(self.bgs[7])
+            self.ax.figure.set_facecolor(self.bgs[8])
+            self.canvas.get_tk_widget().configure(bg=self.bgs[9])
+            self.toolbar.configure(bg=self.bgs[10])
+            for button in self.toolbar.winfo_children():
+                button.configure(bg="#d9d9d9")
+            print(self.bgs)
+            self.canvas.draw()
+            self.graph_frame.configure(bg=self.bgs[11])
+            self.topbar.configure(bg=self.bgs[12])
+            self.is_on = 0
+            print("light!")
+            print(self.is_on)
+
+    def on_closing(self):
+        with open("options",mode="w") as options:
+            options.write(f"is_on={self.is_on}")
+        self.destroy()
 # ----------
 
 # the axis entrys for the functions of FucusIn-and-Out
